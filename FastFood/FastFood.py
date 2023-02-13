7740f12f97c5c2c3ff190c30df4f54cfa9f3c479
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2023-02-12 21:37:25 -0500

    modified fastfood.py

--- FastFood/FastFood.py
+++ FastFood/FastFood.py
@@ -23,18 +23,48 @@ print(len(df))
 # print(df['Fiber (g)'].head(25))
 # print(res.head(25))
 
+df_cats = df.groupby('Category')
+gor = df_cats.get_group('GOURMET MENU')
+mccafe = df_cats.get_group('McCAFE MENU')
+
+print(mccafe)
+
+
+df = df.replace({'Hot Breakfast':'Breakfast','BREAKFAST MENU':'Breakfast','GOURMET MENU':'Drinks'})
+print(df['Category'].value_counts())
+
+sys.exit()
+
 #unpivotting some columns
-df1 = pd.melt(df, id_vars=['Company','Product'], value_vars=['Protein (g)', 'Fiber (g)'])
+df1 = pd.melt(df, id_vars=['Company','Product'], value_vars=['Protein (g)', 'Fiber (g)','Sugar (g)',
+                                            'Total Fat (g)', 'Saturated Fat (g)', 'Trans Fat (g)',
+                                            'Cholesterol (mg)', 'Sodium (mg)'])
 print(df1)
 
+df1 = df1.groupby(['Company','variable']).mean().reset_index()
+
+
+
+
+
+sys.exit()
 fig = px.bar(
     df1,
     x='Company',
     y='value',
     color = 'variable',
-    barmode = 'stack'
+    barmode = 'stack',
+    title = 'Avg Nutrition'
 )
 fig.show()
+
+
+# fig = px.bar(df1.pivot(columns='Company',
+#     values='value').sum(),
+#     title='Protein Per Company',
+#     color= 'variable'
+#     ).update_layout()
+# fig.show()
 
 
 
