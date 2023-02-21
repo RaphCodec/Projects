12f7e0586adf9f1c118ffaf518c44d284aa4c66e
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2023-02-20 20:23:16 -0500

    Added WebScraping -  WWE Champs

---
+++ WebScraping/WWEChamps/wwechamps.py
@@ -0,0 +1,131 @@
+import mechanicalsoup
+import pandas as pd
+import sqlite3
+import time
+
+def mk_dfs(data,to_remove,cols):
+    indices = [idx for idx, x in enumerate(data) if x in to_remove]
+
+    df_lst = []
+    for idx,x in enumerate(indices):
+        if idx == 0:
+            cop = data.copy()
+            cop = cop[:x -1]
+            df_lst.extend(cop)
+        elif x == indices[-1]:
+            cop = data.copy()
+            cop = cop[x+1:]
+            df_lst.extend(cop)
+        else:
+            cop = data.copy()
+            cop = cop[indices[idx-1] + 1 : x-1]
+            df_lst.extend(cop)
+
+    df_dict =  {} 
+
+    for idx, key in enumerate(cols):
+        df_dict[key] = df_lst[idx:][::9] 
+
+    df = pd.DataFrame(df_dict)
+
+    return df
+
+def repeat_values(num, remove_last = True):
+    if remove_last == False:
+        return ('?,' * num)
+    else:
+        return ('?,' * num)[:-1]
+    
+def insert(table,df,vals,cols):
+    #create and connect to sqlite3 database
+    conn = sqlite3.connect('WWEChamps.db')
+    cur = conn.cursor()
+    #create table
+    cur.execute(f'Create Table {table} ({cols})')
+
+    #insert data into table
+    for i in range(len(df)):
+        cur.execute(f'''Insert into {table}
+                        values({vals}
+                        )''', df.iloc[i])
+        
+    conn.commit()
+    conn.close #close conncection
+
+def main():
+    url = 'https://en.wikipedia.org/wiki/List_of_WWE_Champions'
+
+    #making broswer object and opening url
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open(url)
+
+    #getting all the data
+    td = browser.page.find_all('td')
+    browser.close() #closing browser
+
+    data  = [value.text.replace('\n','') for value in td]
+
+    #slicing list to get data for each wwe championship change
+    reigns = data[data.index('Buddy Rogers'):data.index('[204]')+1] 
+    
+    '''deleting specific unnecessary - from data so that it can be converted to 
+        a dataframe later.  These are the - found in the No. column on the webpage
+         table but have a <td> tag.
+        DO NOT TRY TO REPLACE ALL -
+        THERE ARE SOME THAT ARE NEEDED TO COMPLETE ROWS'''
+    del reigns[reigns.index('Antonio Inoki') + 9]
+    del reigns[reigns.index('Ted DiBiase') + 9]
+    del reigns[reigns.index('December 3, 1991') + 8]
+    del reigns[reigns.index('January 19, 1997') + 8]
+    del reigns[reigns.index('June 29, 1998') + 8]
+    del reigns[reigns.index('September 14, 1999') + 8]
+    del reigns[reigns.index('September 17, 2006') + 8]
+    del reigns[reigns.index('June 7, 2009') + 8]
+    del reigns[reigns.index('July 17, 2011') + 8]
+    del reigns[reigns.index('September 15, 2013') + 8]
+    del reigns[reigns.index('April 6, 2014') + 8]
+    del reigns[reigns.index('March 29, 2015') + 8]
+
+
+    reigns=list(filter(lambda a: a != '†', reigns))
+
+    to_remove = ['World Wide Wrestling Federation (WWWF)',
+                'National Wrestling Alliance: World Wide Wrestling Federation (WWWF)',
+                'National Wrestling Alliance: World Wrestling Federation (WWF)',
+                'World Wrestling Federation (WWF)',
+                'World Wrestling Entertainment (WWE)',
+                'WWE: SmackDown',
+                'WWE: ECW',
+                'WWE: Raw',
+                'WWE (unbranded)']
+ 
+    cols = ['Champion',
+            'Date',
+            'Event',
+            'Location',
+            'Reign',
+            'Days',
+            'DaysRecog',
+            'Notes'
+            ]
+
+    df = mk_dfs(reigns,to_remove, cols)
+
+    vals = repeat_values(8)
+
+    cols = ','.join(cols)
+    
+    insert('Champs',df,vals,cols)
+
+
+
+if __name__ == '__main__':
+    start = time.time()
+    try:
+        main()
+        executionTime = (time.time()- start)
+        print(f'Execution time in seconds: {executionTime}')
+    except Exception as e:
+        print(e)
+        executionTime = (time.time()- start)
+        print(f'Execution time in seconds: {executionTime}')
