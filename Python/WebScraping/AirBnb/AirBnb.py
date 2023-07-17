f5455fea25a1327db5b1f41af8b7bacc26680683
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2023-07-17 19:59:28 -0400

    Added AirBnb Webscraping

---
+++ Python/WebScraping/AirBnb/AirBnb.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://insideairbnb.com/get-the-data/")
+    with page.expect_download() as download_info:
+        page.get_by_role("row", name="Amsterdam listings.csv.gz Detailed Listings data").get_by_role("link").click()
+    download = download_info.value
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
